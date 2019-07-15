637e9b4bbeacab96d3ec3dd8e03113707744d32c
Callista <theevenstarlight@gmail.com>
2019-07-14 21:16:04 -0400

    first commit

---
+++ UnitTestingPractice/Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnitTestingPractice
+{
+    public class Stack<T>
+    {
+        //#region Members
+        private T[] stackArray;
+        private int maximumLength;
+        //#endregion
+
+        //#region Properties
+        public int Size { get; private set; }
+        //#endregion
+
+        //#region Constructor
+        public Stack(int length)
+        {
+            maximumLength = length;
+            stackArray = new T[length];
+        }
+        //#endregion
+
+    }
+}
---
+++ UnitTestingPractice/StackTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace UnitTestingPractice
+{
+    [TestFixture]
+    public class StackTest
+    { 
+        [Test]
+        public void Creation()
+        {
+            Stack<int> s = new Stack<int>(3);
+            Assert.AreEqual(0, s.Size);
+        }
+
+    }
+}
